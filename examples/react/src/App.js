f52222131982247353ed59e81be5e50905e1ece8
richarepo <richa@repozitory.com>
2023-11-10 11:59:47 +0530

    added examples

---
+++ examples/react/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import 'recommendation-service';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="search-wrapper">
+          <div className="search-results" id="results">
+            Click this link/button to open Sizey recommendation popup
+          </div>
+          <br />
+          <span className="SizeyContainer" 
+               id="sizey-container" 
+               data-upc={1234567890}
+               apikey='testapikey'
+               recommendation_link_text='Test your size'
+               recommendation_button_text='Test My Size'
+               showaslink='true'>
+            <div data-product-variations>
+              <div data-variation-id="{VariationId}" data-variation-ean="{VariationArticleNumber}"></div>
+            </div>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
